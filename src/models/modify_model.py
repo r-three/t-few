9dbc9cc429888a0c27fc22188b4e9549e0e83f40
Derek Tam <dt.derek.tam@gmail.com>
2022-05-16 14:15:53 -0400

    move import intrinsic to only call on said

--- src/models/modify_model.py
+++ src/models/modify_model.py
@@ -2,19 +2,20 @@ from .lora import modify_with_lora
 from .adapters import modify_with_adapters
 from .bitfit import modify_with_bitfit
 from .prompt_tuning import modify_with_prompt_tuning
-from .intrinsic import modify_with_intrinsic_model
 
 modifier_dict = {
     "lora": modify_with_lora,
     "bitfit": modify_with_bitfit,
     "adapters": modify_with_adapters,
     "prompt-tuning": modify_with_prompt_tuning,
-    "intrinsic": modify_with_intrinsic_model,
 }
 
 
 def modify_transformer(transformer, config):
     if config.model_modifier:
+        if config.model_modifier == "intrinsic":
+            from .intrinsic import modify_with_intrinsic_model
+            transformer = modify_with_intrinsic_model(transformer, config)
         if config.model_modifier in modifier_dict:
             transformer = modifier_dict[config.model_modifier](transformer, config)
         else:
